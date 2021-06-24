0ea801f7a652d17c868c126283dc92f534c03e3e
furkancengizf <78604026+furkancengizf@users.noreply.github.com>
2021-06-24 20:00:32 +0300

    Day7

---
+++ src/main/java/kodlamaio/hrms/business/Abstract/EmployerService.java
@@ -0,0 +1,14 @@
+package hrms.management.business.abstracts;
+
+import hrms.management.core.utilities.results.DataResult;
+import hrms.management.core.utilities.results.Result;
+import hrms.management.entities.concretes.Employer;
+import hrms.management.entities.concretes.JobTitle;
+
+import java.util.List;
+
+public interface EmployerService {
+    DataResult<List<Employer>> getAll();
+
+    Result add(Employer employer);
+}
